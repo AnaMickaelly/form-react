eff16aa65a838e2492aa174ba1d88819f159cbbd
AnaMickaelly <anamickaellyd2@gmail.com>
2022-02-05 16:29:02 -0300

    feat: form with useState

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import { Container } from './styles';
 import LoginImage from '../../assets/Images/Login.svg';
 import { Input } from '../Input';
 
 export function Form() {
+  const [name, setName] = useState('');
+  const [dateOfBirth, setdateOfBirth] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [cep, setCep] = useState('');
+  console.log(cep);
   return (
     <Container>
       <div className="header">
@@ -13,10 +19,30 @@ export function Form() {
         <img src={LoginImage} alt="Login" />
       </div>
       <div className="form-container">
-        <Input title="Nome" placeholder="nome" />
-        <Input title="Data de nascimento" placeholder="00/00/0000" />
-        <Input title="CPF" placeholder="000-000-000-00" />
-        <Input title="CEP" placeholder="00-000-000" />
+        <Input
+          title="Nome"
+          placeholder="nome"
+          value={name}
+          onChange={(event: any) => setName(event.target.value)}
+        />
+        <Input
+          title="Data de nascimento"
+          placeholder="00/00/0000"
+          value={dateOfBirth}
+          onChange={(event: any) => setdateOfBirth(event.target.value)}
+        />
+        <Input
+          title="CPF"
+          placeholder="000-000-000-00"
+          value={cpf}
+          onChange={(event: any) => setCpf(event.target.value)}
+        />
+        <Input
+          title="CEP"
+          placeholder="00-000-000"
+          value={cep}
+          onChange={(event: any) => setCep(event.target.value)}
+        />
         <div className="button-container">
           <button>Enviar</button>
         </div>
